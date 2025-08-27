829f958f88f9a5a9d17e2b3026403be4909f2694
HaKu <335279429@qq.com>
2025-08-28 00:03:06 +0800

    Initial commit: Android app skeleton with gitignore

---
+++ Anya/app/src/main/java/com/haku/anya/EmptyActivity.kt
@@ -0,0 +1,11 @@
+package com.haku.anya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EmptyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 这是一个空页面，没有设置布局
+    }
+}
---
+++ build_apk.bat
@@ -0,0 +1,9 @@
+@echo off
+echo 正在创建APK构建脚本...
+echo.
+echo 请确保已安装Android Studio并配置好环境变量
+echo 或者使用Android Studio打开Anya文件夹进行构建
+echo.
+echo APK文件将生成在: Anya\app\build\outputs\apk\debug\app-debug.apk
+echo.
+pause
